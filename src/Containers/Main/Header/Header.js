1de79fa3cfd7e829f202971f7ec27a3db9000e24
Hayk <hayk.hovsepyan98@gmail.com>
2019-10-20 15:29:13 +0400

    adding css modules

---
+++ src/Containers/Main/Header/Header.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import classes from './Header.scss';
+
+class Header extends Component {
+    render() {
+        return (
+            <div className={classes.header}>
+                
+            </div>
+        );
+    }
+}
+
+export default Header;

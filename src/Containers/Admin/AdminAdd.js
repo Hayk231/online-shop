425ca444b3a1eccd4f136c7dbb30c79162fc8c53
Hayk231 <hayk.hovsepyan98@gmail.com>
2019-10-28 21:36:07 +0400

    changes

--- src/Containers/Admin/AdminAdd.js
+++ src/Containers/Admin/AdminAdd.js
@@ -1,7 +1,18 @@
 import React, {Component} from 'react';
 import classes from "./Admin.scss";
+import firebase from 'firebase';
 
 class AdminAdd extends Component {
+
+    addProductHandler = () => {
+        firebase.database().ref('/products').push({
+            name: this.nameRef.value,
+            info: this.descRef.value,
+            price: this.priceRef.value,
+            imageUrl : ''
+        }).then(res => console.log(res))
+    };
+
     render() {
         return (
             <div className={classes.admin_add_par}>
@@ -11,10 +22,10 @@ class AdminAdd extends Component {
                         <input type='file'/>
                     </label>
                     <div className={classes.admin_add_inputs}>
-                        <input type='text'/>
-                        <textarea></textarea>
-                        <input type='number'/>
-                        <button>Add</button>
+                        <input type='text' placeholder='Product name' ref={ref => this.nameRef = ref}/>
+                        <textarea placeholder='Product info' ref={ref => this.descRef = ref}></textarea>
+                        <input type='number' placeholder='Price' step={1000} ref={ref => this.priceRef = ref}/>
+                        <button onClick={this.addProductHandler}>Add</button>
                     </div>
                 </div>
             </div>
--- src/Containers/Admin/AdminPanel.js
+++ src/Containers/Admin/AdminPanel.js
@@ -1,16 +1,28 @@
 import React, {Component} from 'react';
 import classes from './Admin.scss';
 import AdminAdd from "./AdminAdd";
-class AdminPanel extends Component {
-    render() {
+import firebase from 'firebase';
 
-        let arr = [
-            {image : 'https://cdn.pixabay.com/photo/2017/09/09/18/25/living-room-2732939_960_720.jpg', name: 'name', desc: 'description', price: '500000'},
-            {image : 'https://cdn.pixabay.com/photo/2017/09/09/18/25/living-room-2732939_960_720.jpg', name: 'name', desc: 'description', price: '500000'},
-            {image : 'https://cdn.pixabay.com/photo/2017/09/09/18/25/living-room-2732939_960_720.jpg', name: 'name', desc: 'description', price: '500000'},
-            {image : 'https://cdn.pixabay.com/photo/2017/09/09/18/25/living-room-2732939_960_720.jpg', name: 'name', desc: 'description', price: '500000'},
-            {image : 'https://cdn.pixabay.com/photo/2017/09/09/18/25/living-room-2732939_960_720.jpg', name: 'name', desc: 'description', price: '500000'}
-        ];
+
+class AdminPanel extends Component {
+
+    state = {
+        products: []
+    };
+
+    componentDidMount() {
+        let ref = firebase.database().ref('/products');
+        ref.on('value', snapshot => {
+            const products = snapshot.val();
+            const finArr = [];
+            for (let key in products) {
+                finArr.push({id: key, info : products[key]})
+            }
+            this.setState({products: finArr});
+        });
+    }
+
+    render() {
 
         return (
             <div className={classes.admin_panel}>
@@ -28,13 +40,13 @@ class AdminPanel extends Component {
                         </thead>
                         <tbody>
                             {
-                                arr.map(el => {
+                                this.state.products.map(el => {
                                     return (
-                                        <tr>
-                                            <td><img src={el.image} style={{height: '30px', width: '40px'}} alt='asd'/></td>
-                                            <td>{el.name}</td>
-                                            <td>{el.desc}</td>
-                                            <td>{el.price}</td>
+                                        <tr key={el.id}>
+                                            <td><img src={el.info.imageUrl} style={{height: '30px', width: '40px'}} alt='asd'/></td>
+                                            <td>{el.info.name}</td>
+                                            <td>{el.info.info}</td>
+                                            <td>{el.info.price}</td>
                                         </tr>
                                     )
                                 })

91ff20315a854334998c1760a7b90571c600a2bc
Hayk231 <hayk.hovsepyan98@gmail.com>
2020-02-17 17:20:38 +0400

    Updates

---
+++ precache-manifest.a6cc0ae4120b0f34e248775e0e25bcd7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33d946cff52adbbfa65d25275620f09f",
+    "url": "/online-shop/index.html"
+  },
+  {
+    "revision": "af40a55c6b7fe8288bf5",
+    "url": "/online-shop/static/css/main.3391b517.chunk.css"
+  },
+  {
+    "revision": "96ecebc52e273d6f2783",
+    "url": "/online-shop/static/js/2.7d3f3689.chunk.js"
+  },
+  {
+    "revision": "af40a55c6b7fe8288bf5",
+    "url": "/online-shop/static/js/main.83b47472.chunk.js"
+  },
+  {
+    "revision": "e46423944571c7b9a951",
+    "url": "/online-shop/static/js/runtime~main.cbb09e6a.js"
+  }
+]);
